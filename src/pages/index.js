bb353b8aec8d475b97e6242dfc2061cf00959ed1
Luka Stanisavljevic <luka@folkmatic.com>
2018-11-15 15:45:34 +0100

    Čekaj malo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
+// import Image from '../components/image'
 import Klavir from '../components/Klavir'
 
 const IndexPage = () => (
