4ad092ff73bab741ec3535863110544f972019f2
Luka Stanisavljevic <luka@folkmatic.com>
2018-11-19 16:05:33 +0100

    Ajde

--- src/components/header.js
+++ src/components/header.js
@@ -4,17 +4,18 @@ import { Link } from 'gatsby'
 const Header = ({ siteTitle }) => (
   <div id="glava" className="glava">
 
-          <h1 style={{ margin: 0 }}>
-              <Link
-                  to="/"
-                  style={{
+      <h1 style={{ margin: 0 }}>
+          <Link
+              to="/"
+              style={{
                       color: 'white',
                       textDecoration: 'none',
-                  }}
-              >
-                  {siteTitle}
-              </Link>
-          </h1>
+              }}
+          >
+              {siteTitle}
+          </Link>
+      </h1>
+      <div>React.js synthesizer</div>
 
   </div>
 )
